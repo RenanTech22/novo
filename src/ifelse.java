d16e8557f600c81e71167083990ac47890ebd751
RenanTech22 <renanrodrigues7303@gmail.com>
2025-04-08 22:29:28 -0300

    ja pingou ai

--- src/ifelse.java
+++ src/ifelse.java
@@ -5,31 +5,36 @@ public class ifelse {
 
         Scanner sc = new Scanner(System.in);
 
-        int p1, p2, t1, t2, t3, mf;
+        double p1, p2, t1, t2, t3, mf;
 
         System.out.println("Quanto você tirou na primeira prova? ");
-        p1 = sc.nextInt();
+        p1 = sc.nextDouble();
 
         System.out.println("Quanto você tirou na segunda prova? ");
-        p2 = sc.nextInt();
+        p2 = sc.nextDouble();
 
         System.out.println("Quanto você tirou no primeiro trabalho? ");
-        t1 = sc.nextInt();
+        t1 = sc.nextDouble();
 
         System.out.println("Quanto você tirou no segundo trabalho? ");
-        t2 = sc.nextInt();
+        t2 = sc.nextDouble();
 
         System.out.println("Quanto você tirou no terceiro trabalho? ");
-        t3 = sc.nextInt();
+        t3 = sc.nextDouble();
 
         mf = (((p1+p2)/2)*7+((t1+t2+t3)/3)*3)/10;
 
         if (mf>=6) {
-            System.out.println("Você tirou "+mf+" então foi aprovado");
+            System.out.printf("Você tirou %.2f então foi aprovado",mf);
         }
         else {
-            System.out.println("Você tirou "+mf+" então foi reprovado");
+            System.out.printf("Você tirou %.2f então foi reprovado, seu fudido.",mf );
         }
 
     }
 }
+
+
+
+git commit -m "ja pingou ai"
+git push
